97630eb722887dd9c3aa73adb640e26b1fd8c9f5
[Manojkumar1710] <[kumarakulamanoj7@gmail.com]>
2026-06-14 18:39:28 +0530

    updated SecurityConfig file

--- src/main/java/com/manoj/inventory/config/SecurityConfig.java
+++ src/main/java/com/manoj/inventory/config/SecurityConfig.java
@@ -8,6 +8,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
 @Configuration
 public class SecurityConfig {
 
@@ -25,6 +31,7 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
 
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(
@@ -54,7 +61,6 @@ public class SecurityConfig {
                                 "/api/categories/**",
                                 "/api/suppliers/**",
                                 "/api/dashboard/**"
-
                         ).hasAnyRole(
                                 "ADMIN",
                                 "MANAGER")
@@ -70,5 +76,32 @@ public class SecurityConfig {
                 .httpBasic(httpBasic -> httpBasic.disable());
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration =
+                new CorsConfiguration();
+
+        configuration.setAllowedOrigins(
+                List.of("http://localhost:5173"));
+
+        configuration.setAllowedMethods(
+                List.of("*"));
+
+        configuration.setAllowedHeaders(
+                List.of("*"));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+                "/**",
+                configuration);
+
+        return source;
     }
 }
